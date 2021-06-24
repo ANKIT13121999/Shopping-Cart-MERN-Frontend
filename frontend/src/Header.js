9f4122da1c59e5fa7bb8f0b01de5b1013ccf5ab8
ANKIT13121999 <77962719+ANKIT13121999@users.noreply.github.com>
2021-06-24 15:42:40 +0530

    added header

---
+++ frontend/src/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from './Header';
+
+function Header() {
+    return (
+        <div>
+            i am header
+        </div>
+    )
+}
+
+export default Header
